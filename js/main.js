0b79d76e5c861460a9f77c88075a6ca02d31e0d2
Valeria Oshiro <valeriaoshiro19@gmail.com>
2017-06-27 16:46:36 -0700

    Create board and insert bombs

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 /*----- app's state (variables) -----*/
+var board, randomBombs;
+
 /*----- cached element references -----*/
 var squares = document.querySelectorAll('td');
 
@@ -14,11 +16,22 @@ function init(){
         el.classList.remove('clicked');
         el.classList.add('unclicked');
     });
+    board = [];
+    for(var i = 0; i < 64; i++){
+        board[i] = null;
+    }
+    randomBombs = [];
+    for(var i = 0; i < 10; i++){
+        randomBombs.push(Math.floor(Math.random()*64));
+    }
+    console.log(randomBombs);
+    randomBombs.forEach(function(el){
+        board[el] = 'bomb';
+    });
+    console.log(board);
 }
 
 function handleSquareClick(evt){
     evt.target.classList.remove('unclicked');
     evt.target.classList.add('clicked');
 }
-
-// Math.floor(Math.random()*63)
