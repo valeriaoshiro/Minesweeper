71e537e627d3c02e04f6363710e6ef13635da777
Valeria Oshiro <valeriaoshiro19@gmail.com>
2017-06-27 18:51:21 -0700

    Change squares to be represented as object

--- js/main.js
+++ js/main.js
@@ -6,27 +6,47 @@ var squares = document.querySelectorAll('td');
 
 /*----- event listeners -----*/
 document.querySelector('tbody').addEventListener('click', handleSquareClick);
-document.querySelector('button').addEventListener('button', init);
+document.querySelector('button').addEventListener('click', init);
 
 /*----- functions -----*/
 init();
 
 function init(){
+    // Makes the square dark gray and clears all the bomb icons
     squares.forEach(function(el){
         el.classList.remove('clicked');
         el.classList.add('unclicked');
+        el.innerHTML = '';
     });
-    board = [];
-    for(var i = 0; i < 64; i++){
-        board[i] = null;
-    }
+    // Create an array with 10 indexes where the bombs are going
     randomBombs = [];
     for(var i = 0; i < 10; i++){
-        randomBombs.push(Math.floor(Math.random()*64));
+        var random = Math.floor(Math.random()*64);
+        if(!randomBombs.includes(random)){
+            randomBombs.push(random);
+        } else {
+            i--;
+        }
+    }
+    // Create the array object with display false, its coordinate, and value of bomb
+    board = [];
+    var xcoor = 0;
+    var ycoor = 0;
+    for(var i = 0; i < 64; i++){
+        var eachSquare = {
+            display: false,
+            coor: {x: xcoor, y: ycoor}
+        };
+        board.push(eachSquare);
+        xcoor++;
+        if(xcoor > 7){
+            ycoor++;
+            xcoor = 0;
+        }
     }
     console.log(randomBombs);
     randomBombs.forEach(function(el){
-        board[el] = 'bomb';
+        board[el].value = 'bomb';
     });
     console.log(board);
     render();
@@ -39,7 +59,7 @@ function handleSquareClick(evt){
 
 function render(){
     board.forEach(function(el, index){
-        if(el === 'bomb'){
+        if(el.value === 'bomb'){
             squares[index].innerHTML = '<i class="fa fa-bomb" aria-hidden="true"></i>';
         }
     });
--- pseudocode.md
+++ pseudocode.md
@@ -4,18 +4,18 @@
 * Add event listeners to the board
 * Render board
     * Board will be 8x8 with 10 bombs
-    * Square can be empty unclicked
-    * Square can be empty clicked
-    * Square can be bomb
-    * Square can be a number
 * Wait for the user to click
     * if reset (happy face), then initialize
-    * if click on a square 
-        * if first time clicking - start timer
-        * if empty - open other surrounding empty squares and numbers
-        * if bomb - game over, change to sad face
-        * if number - show number
+    * if click on a square
+        * if diplay is clicked, do nothing
+        * if display is unclicked:
+            * if first time clicking - start timer
+            * if state of board is empty - open other surrounding empty squares and numbers
+            * if state of board is bomb - game over, change to sad face
+            * if state of board is number - show number
     * if shift-click on a square
+        * if display is clicked, do nothing
+        * 
         * if empty - show flag, decrease bomb number
         * if has flag - show ?, increase bomb number
         * if has ? - show empty
@@ -24,11 +24,8 @@
 
 
 ## Variables
-* display squares
-    * 0 - un-click
-    * 1 - empty clicked
-    * 2 - bomb
-    * 3 - number
-    * 4 - flag
-    * 5 - ?
-* state of 
+* board (array)
+    * contains object of each square 
+        * display - false if not clicked, true if clicked
+        * value - bomb, number, null
+        * coor - {x, y}
